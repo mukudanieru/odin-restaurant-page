3300701ca8cf2c5704a24988dff547f80f0c7982
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-11-11 11:50:30 +0800

    Add basic homepage layout - Created a homepage structure inside div content - Implemented responsive grid layout for image and text sections - Hard-coded initial homepage content for testing layout and visuals

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
 import "../css/styles.css";
+import brothersLogo from "../asssets/logo.png";
 
 console.log("TEST");
+
+// const content = document.querySelector("#content");
+const image = document.querySelector("#image");
+image.src = brothersLogo;
